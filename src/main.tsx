525dc5eee5e7c357110501edb977d55381cffa45
Tahsin <alahitahsin@gmail.com>
2024-11-21 01:27:12 +0600

    created: new route

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import AppLayout from "./AppLayout.tsx";
+import { RouterProvider } from "react-router-dom";
+import router from "./RouterRoutes";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>{/* <App /> */}</StrictMode>
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
 );

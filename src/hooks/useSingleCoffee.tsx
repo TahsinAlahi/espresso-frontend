5695d5ca3d7dce22027f9217a6827d02b2378335
Tahsin <alahitahsin@gmail.com>
2024-11-27 19:29:48 +0600

    fixed: useSingleCoffee

--- src/hooks/useSingleCoffee.tsx
+++ src/hooks/useSingleCoffee.tsx
@@ -1,19 +1,23 @@
 import { useEffect, useState } from "react";
 
 function useSingleCoffee(id: string) {
-  const [coffee, setCoffee] = useState<CoffeeType>({} as CoffeeType);
+  const [coffee, setCoffee] = useState<CoffeeType | null>(null);
 
   useEffect(() => {
+    if (!id) return;
+
     async function getCoffee() {
       try {
         const res = await fetch(
           `${import.meta.env.VITE_BASE_URL}/api/coffees/${id}`
         );
+
+        if (!res.ok) throw new Error(await res.json());
+
         const coffee = await res.json();
         setCoffee(coffee);
       } catch (error) {
         console.log(error);
-        alert(error);
       }
     }
 
